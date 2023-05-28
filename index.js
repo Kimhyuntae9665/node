56c1400b22974c66b23a93f4be219d433ba4bbff
Kimhyuntae9665 <Kimhyuntae9665>
2023-05-28 12:35:55 +0900

    module cors 사용 모든 html의 접근을 차단

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
+
+var cors = require('cors')
+
 const app = express()
+
+// app.use(cors())
 
 app.get('/', function (req, res) {
     req = "whatup";
   res.send(req);
 })
+
+app.get('/sound/:name', (req, res)=>{
+  const {name} = req.params;
+
+  if(name=="dog"){
+    res.json({'sounds':'멍멍'})
+  } else if(name=="cat"){
+    res.json({'sounds':'야옹'})
+  } else if(name=="pig"){
+    res.json({'sounds':'꿀꿀'})
+  } else{
+    res.json({'sounds':'알 수 없음'})
+  }
+
+})
+
+app.get('/cat', function (req, res) {
+  req = "<h1>Eiyahong</h1>";
+res.send(req);
+})
+
+
 
 app.listen(3000)
